1ebc52caffbd73c1f55a2c0228dab18bdd2d9acf
AvishkarMandlik <mandlikavi121gmail.com>
2022-10-31 04:49:05 -0400

    all js function complete

--- javascript/bulb-onoff.js
+++ javascript/bulb-onoff.js
@@ -1,6 +1,12 @@
-function bulbOn(){
+function bulbOn() {
     document.getElementById("bulb").src = "./images/bulb-on.png";
+    document.getElementById("btn-on").className = "btn btn-on";
+    document.getElementById("btn-off").className = "btn";
+    document.querySelector(".card").className = "card card-light";
 }
-function bulbOff(){
+function bulbOff() {
     document.getElementById("bulb").src = "./images/bulb-off.png";
+    document.getElementById("btn-off").className = "btn btn-off";
+    document.getElementById("btn-on").className = "btn";
+    document.querySelector(".card").className = "card card-dark";
 }
